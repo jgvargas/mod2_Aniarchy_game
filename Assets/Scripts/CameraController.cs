7c2a2f67ca638124f858de39606dc413410fc91a
jgvargas <juan.vargas01@utrgv.edu>
2017-04-11 18:27:55 -0500

    A of April 11

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿/* Third person camera rig: followers player in a third person
+ * 
+ * credit: code.tutsplus.com/tutorials
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	public Vector3 offset;
+
+	//Used to adjust damping of camera angle and player orientation
+	public float damping = 1;
+
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	void LateUpdate ()
+	{	
+		// Old implementation
+		transform.position = player.transform.position + offset;
+		/*
+		float currentAngle = transform.eulerAngles.y;
+		
+		float wantedAngle = player.transform.eulerAngles.y;
+		float angle = Mathf.LerpAngle( currentAngle, wantedAngle, Time.deltaTime* damping);
+
+		Quaternion rotation = Quaternion.Euler(0, wantedAngle, 0);
+		transform.position = player.transform.position - (rotation * offset);
+
+		transform.LookAt(player.transform);
+
+*/
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text scoreText;
+	public Text winText;
+
+	public float jumpSpeed = 100.0f;
+	private bool onGround = false;
+
+	// Private: only accessible from script
+	private int score_count;
+
+	// Physics component
+	public Rigidbody rigidbody_ref;
+
+	void Start()
+	{
+		score_count = 0;
+		rigidbody_ref = GetComponent<Rigidbody> ();
+		SetCountText ();
+		winText.text = "";
+	}
+
+	//Update: Called before a frame is rendered. 
+	void Update()
+	{
+		if (Input.GetKeyDown("space"))
+		{
+			rigidbody_ref.AddForce(Vector3.up * jumpSpeed);
+		}
+	}
+
+	//FixedUpdate: Before any physics is applied
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Ribidbody: class, .AddForce to move RB
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody_ref.AddForce (movement * speed);
+
+	}
+
+	void OnTriggerEnter( Collider other)
+	{
+		if (other.gameObject.CompareTag("PickUp"))
+		{
+			other.gameObject.SetActive( false);
+			score_count = score_count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText()
+	{
+		scoreText.text = "Count: " + score_count.ToString ();
+		if (score_count >= 3) {
+			winText.text = "You Win!";
+		}
+	}
+
+	void OnCollisionStay ()
+	{
+		onGround = true;
+	}
+}
+
